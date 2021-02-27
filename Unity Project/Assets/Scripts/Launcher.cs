992a5c9a0019abc005b615ba11da22b9bb02b0cf
Connor Boerma <00conman@gmail.com>
2021-02-27 12:10:17 -0500

    Prototype Room Finding

--- Unity Project/Assets/Scripts/Launcher.cs
+++ Unity Project/Assets/Scripts/Launcher.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListButtonPrefab;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
     void Start()
     {
         Debug.Log("Connecting to Master");
@@ -56,8 +64,25 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("Connecting");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("Connecting");
+    }
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("MainMenu");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListButtonPrefab, roomListContent).GetComponent<RoomListButton>().SetUp(roomList[i]);
+        }
     }
 }
---
+++ Unity Project/Assets/Scripts/RoomListButton.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RoomListButton : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+
+    RoomInfo info;
+    public void SetUp(RoomInfo parInfo)
+    {
+        info = parInfo;
+        text.text = info.Name;
+    }
+
+    public void OnClick()
+    {
+        Launcher.Instance.JoinRoom(info);
+    }
+}
