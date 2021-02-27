a8b263d3082265a53ee46b3508e43375bcfd6692
Connor Boerma <00conman@gmail.com>
2021-02-27 16:18:42 -0500

    Full multiplayer functionallity for movement. Players can connect to a room and then move about in real time and collide with each other.

--- Unity Project/Assets/Scripts/PlayerController.cs
+++ Unity Project/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject cameraHolder;
+
+    [SerializeField] float mouseSenstivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+
+    float verticalLookRotation;
+    bool grounded;
+    Vector3 smoothMoveVelocity;
+    Vector3 moveAmount;
+
+    Rigidbody rb;
+
+    PhotonView PV;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        PV = GetComponent<PhotonView>();
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        if (!PV.IsMine)
+        {
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+            Destroy(rb);
+        }
+    }
+
     void Update()
     {
-        
+        if (!PV.IsMine)
+            return;
+        Look();
+        Move();
+        Jump();
+    }
+
+    private void Look()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSenstivity);
+
+        //note the minus sign can be changed to a plus sign to invert mouse movement
+        verticalLookRotation -= Input.GetAxisRaw("Mouse Y") * mouseSenstivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+    }
+
+
+    private void Move()
+    {
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+    public void SetGroundedState(bool parGrounded)
+    {
+        grounded = parGrounded;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!PV.IsMine)
+            return;
+        rb.MovePosition(rb.position + (transform.TransformDirection(moveAmount) * Time.fixedDeltaTime));
     }
 }
