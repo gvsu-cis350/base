7da0e646d2578c170321e743df44bc1dfd1fdf42
Connor Boerma <00conman@gmail.com>
2021-03-03 20:16:18 -0500

    Added guns, damage, death, respawning.

--- Unity Project/Assets/Scripts/PlayerController.cs
+++ Unity Project/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+using Photon.Realtime;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks, IDamageable
 {
     [SerializeField] GameObject cameraHolder;
 
     [SerializeField] float mouseSenstivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+
+    [SerializeField] Item[] items;
+
+    int itemIndex;
+    int previousItemIndex = -1;
 
     float verticalLookRotation;
     bool grounded;
@@ -18,15 +25,26 @@ public class PlayerController : MonoBehaviour
 
     PhotonView PV;
 
+    const float maxHealth = 100f;
+    float currentHealth = maxHealth;
+
+    PlayerManager playerManager;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         PV = GetComponent<PhotonView>();
+
+        playerManager = PhotonView.Find((int)PV.InstantiationData[0]).GetComponent<PlayerManager>();
     }
 
     private void Start()
     {
-        if (!PV.IsMine)
+        if (PV.IsMine)
+        {
+            EquipItem(0);
+        } 
+        else
         {
             Destroy(GetComponentInChildren<Camera>().gameObject);
             Destroy(rb);
@@ -40,6 +58,41 @@ public class PlayerController : MonoBehaviour
         Look();
         Move();
         Jump();
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString())){
+                EquipItem(i);
+                break;
+            }
+        }
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+        {
+            if (itemIndex >= items.Length - 1)
+            {
+                EquipItem(0);
+            }
+            else
+            { 
+                EquipItem(itemIndex + 1);
+            }
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+        {
+            if (itemIndex <= 0)
+            {
+                EquipItem(items.Length - 1);
+            }
+            else 
+            {
+                EquipItem(itemIndex - 1);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            items[itemIndex].Use();
+        }
     }
 
     private void Look()
@@ -69,6 +122,43 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void EquipItem(int parIndex)
+    {
+        if (parIndex == previousItemIndex)
+            return;
+        itemIndex = parIndex;
+
+        items[itemIndex].itemGameObject.SetActive(true);
+
+        if(previousItemIndex != -1)
+        {
+            items[previousItemIndex].itemGameObject.SetActive(false);
+        }
+
+        previousItemIndex = itemIndex;
+
+        //check to see if we are the local player
+        if(PV.IsMine)
+        {
+            //create a new hashtable
+            Hashtable hash = new Hashtable();
+            //add our item index to the hashtable
+            hash.Add("itemIndex", itemIndex);
+            //send the hashtable over the photon network
+            PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+        }
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        //Check to see if we are not the local player (not synced)
+        //and check to see if this function matches to the player that we are calling this for
+        if(!PV.IsMine && targetPlayer == PV.Owner)
+        {
+            //sync weapons if aboce is true
+            EquipItem((int)changedProps["itemIndex"]);
+        }
+    }
     public void SetGroundedState(bool parGrounded)
     {
         grounded = parGrounded;
@@ -80,5 +170,31 @@ public class PlayerController : MonoBehaviour
         if (!PV.IsMine)
             return;
         rb.MovePosition(rb.position + (transform.TransformDirection(moveAmount) * Time.fixedDeltaTime));
+    }
+
+
+    public void TakeDamage(float damage)
+    {
+        PV.RPC("RPC_TakeDamage", RpcTarget.All, damage);
+    }
+
+    [PunRPC]
+    void RPC_TakeDamage(float damage)
+    {
+        if (!PV.IsMine)
+            return;
+
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+        //Debug.Log("took damage: " + damage);
+    }
+
+    void Die()
+    {
+        playerManager.Die();
     }
 }
