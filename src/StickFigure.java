14717f6d2a66a9f02a6127c15823a45140484adc
AbigailM103 <abagel103@gmail.com>
2021-03-23 14:28:35 -0400

    added basic outline

--- src/StickFigure.java
+++ src/StickFigure.java
@@ -6,5 +6,20 @@ public class StickFigure {
    private Color color;
    private int size;
 
+   public void drawing (Graphics paper){
+      int head = feet - size;
 
+      paper.setColor(color); // this sets the color of the stick figure
+      paper.drawOval(); // make the head
+      paper.drawLine(); // make the body
+
+      // make the legs
+      paper.drawLine();
+      paper.drawLine();
+
+      // make the arms
+      paper.drawLine();
+      paper.drawLine();
+
+   }
 }
