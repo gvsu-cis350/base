a3436ea213196dc13cd944fc75b07bf85145c41a
AbigailM103 <abagel103@gmail.com>
2021-03-24 07:25:17 -0400

    basic outline

--- src/StickFigure.java
+++ src/StickFigure.java
@@ -10,16 +10,16 @@ public class StickFigure {
       int head = feet - size;
 
       paper.setColor(color); // this sets the color of the stick figure
-      paper.drawOval(); // make the head
-      paper.drawLine(); // make the body
+      paper.drawOval(body-10, head, 20, 20); // make the head
+      paper.drawLine(body, head+20, head, feet - 30); // make the body
 
       // make the legs
-      paper.drawLine();
-      paper.drawLine();
+      paper.drawLine(body, feet-30, body-15, feet);
+      paper.drawLine(body, feet-30, body+15, feet);
 
       // make the arms
-      paper.drawLine();
-      paper.drawLine();
+      paper.drawLine(body, feet-70, body-25, feet-70);
+      paper.drawLine(body, feet-70, body+25, feet-85);
 
    }
 }
