fa0422fba3b39c546bf81e4bd321b7d9b908c8fb
AbigailM103 <abagel103@gmail.com>
2021-03-24 08:18:10 -0400

    basic outline

--- src/StickFigureDemo.java
+++ src/StickFigureDemo.java
@@ -1,28 +1,38 @@
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
 
+public class StickFigureDemo extends JFrame implements ActionListener {
+    private JPanel viewerPanel;
+    private JPanel controlPanel;
 
-public class StickFigureDemo{
+    private Container contentPane;
 
-   private int body;
-   private int feet;
-   private Color color;
-   private int size;
+    public StickFigureDemo(){
+        super("GUI Dancer");
 
-   public void drawing (Graphics paper){
-      int head = feet - size;
+        contentPane = new Container();
+        contentPane.setLayout(new GridLayout(1,2));
 
-      paper.setColor(color); // this sets the color of the stick figure
-      paper.drawOval(body-10, head, 20, 20); // make the head
-      paper.drawLine(body, head+20, head, feet - 30); // make the body
+        viewerPanel = new JPanel();
+        viewerPanel.setBackground(Color.white);
+        viewerPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        contentPane.add(viewerPanel);
 
-      // make the legs
-      paper.drawLine(body, feet-30, body-15, feet);
-      paper.drawLine(body, feet-30, body+15, feet);
+        add(contentPane);
+    }
 
-      // make the arms
-      paper.drawLine(body, feet-70, body-25, feet-70);
-      paper.drawLine(body, feet-70, body+25, feet-85);
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-   }
+    }
 
+    public static void main(String[] args){
+        StickFigureDemo frame = new StickFigureDemo();
+        frame.setSize(new Dimension(500, 500));
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
