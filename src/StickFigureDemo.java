5d4a16b7ceaeb21b9aad2b849ca7ee2b2ebb4432
AbigailM103 <abagel103@gmail.com>
2021-03-30 19:56:07 -0400

    I am starting a different approach.

--- src/StickFigureDemo.java
+++ src/StickFigureDemo.java
@@ -17,7 +17,7 @@ public class StickFigureDemo extends JFrame {
         //changeable things
         setSize(500, 375);
         setTitle("Stick Figure Demo");
-        //add("Center", figure);
+        add("Center", figure);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //another way to center the screen
@@ -27,25 +27,9 @@ public class StickFigureDemo extends JFrame {
 
     private class StickFigure extends Canvas {
 
-        public void drawing (Graphics paper){
-            int body = 0;
-            int feet = 0;
-            Color color = null;
-            int size = 0;
+        public void drawing (Graphics p){
 
-            int head = feet - size;
-
-            paper.setColor(color); // this sets the color of the stick figure
-            paper.drawOval(body-10, head, 20, 20); // make the head
-            paper.drawLine(body, head+20, head, feet - 30); // make the body
-
-            // make the legs
-            paper.drawLine(body, feet-30, body-15, feet);
-            paper.drawLine(body, feet-30, body+15, feet);
-
-            // make the arms
-            paper.drawLine(body, feet-70, body-25, feet-70);
-            paper.drawLine(body, feet-70, body+25, feet-85);
+            p.drawOval(50,50,100,25); // make the head
 
         }
     }
