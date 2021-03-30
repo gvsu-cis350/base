95922553a456b89412360219093ca53aedcbe33e
AbigailM103 <abagel103@gmail.com>
2021-03-30 16:59:00 -0400

    I am starting a different approach.

--- src/StickFigureDemo.java
+++ src/StickFigureDemo.java
@@ -3,25 +3,29 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 
-public class StickFigureDemo extends JFrame implements ActionListener {
+public class StickFigureDemo extends JFrame {
+
+    private StickFigure figure = new StickFigure();
+
+    public static void main(String[] args){
+        StickFigureDemo fr = new StickFigureDemo();
+    }
+
+    public StickFigureDemo(){
+        setLayout(new BorderLayout());
+        setSize(500, 375);
+    }
+
+    private class StickFigure  extends figure{}
+
+    //other stuff
     private JPanel viewerPanel;
     private JPanel controlPanel;
 
     private Container contentPane;
 
-    public StickFigureDemo(){
-        super("Sitck Figure Demo");
 
-        contentPane = new Container();
-        contentPane.setLayout(new GridLayout(1,1));
 
-        viewerPanel = new JPanel();
-        viewerPanel.setBackground(Color.white);
-        viewerPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        contentPane.add(viewerPanel);
-
-        add(contentPane);
-    }
     public int body;
     public int feet;
     public Color color;
@@ -49,11 +53,5 @@ public class StickFigureDemo extends JFrame implements ActionListener {
 
     }
 
-    public static void main(String[] args){
-        StickFigureDemo frame = new StickFigureDemo();
-        frame.setSize(new Dimension(1000, 1000));
-        frame.setResizable(false);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+
 }
