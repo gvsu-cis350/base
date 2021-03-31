743382357b47ad5c177a9030bae045ebd83ea800
AbigailM103 <abagel103@gmail.com>
2021-03-31 00:05:15 -0400

    Drew a stick figure using canvas

--- src/StickFigureDemo.java
+++ src/StickFigureDemo.java
@@ -1,6 +1,4 @@
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class StickFigureDemo extends JFrame {
@@ -27,9 +25,21 @@ public class StickFigureDemo extends JFrame {
 
     private class StickFigure extends Canvas {
 
-        public void drawing (Graphics p){
+        public void paint (Graphics p){
 
-            p.drawOval(50,50,100,25); // make the head
+            p.drawOval(175,50,90,70); // make the head
+            p.drawLine( 220,120, 220,220 ); //body
+
+            p.drawLine( 220,130, 190,160 );   // Draw top left arm
+            p.drawLine( 220,130, 250,160 );   // Draw top right arm
+            p.drawLine( 200,190, 190,160 );   // Draw bottom left arm
+            p.drawLine( 240,190, 250,160 );   // Draw bottom right arm
+
+            p.drawLine( 220,220, 200,250 );   // Draw top left leg
+            p.drawLine( 220,220, 245,250 );   // Draw top right leg
+
+            p.drawLine( 200,280, 200,250 );   // Draw bottom left leg
+            p.drawLine( 245,280, 245,250 );   // Draw bottom right leg
 
         }
     }
