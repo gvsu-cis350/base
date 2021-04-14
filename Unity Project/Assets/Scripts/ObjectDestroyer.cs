5acfac5675f4a91b3192f72271f3408e695af646
Connor Boerma <00conman@gmail.com>
2021-04-14 11:12:14 -0400

    Added more sound framework and setup some work for linking sounds continously

--- Unity Project/Assets/Scripts/ObjectDestroyer.cs
+++ Unity Project/Assets/Scripts/ObjectDestroyer.cs
@@ -3,18 +3,32 @@
 /// Author: MutantGopher
 /// This script destroys a GameObject after the number of seconds specified in
 /// the lifeTime variable.  Useful for things like explosions and rockets.
+/// 
+/// Extra Pun additions made by Connor Boerma
 /// </summary>
 
 using UnityEngine;
 using System.Collections;
+using Photon.Pun;
 
-public class TimedObjectDestroyer : MonoBehaviour
+
+public class ObjectDestroyer : MonoBehaviour
 {
+	public bool signal = false;
 	public float lifeTime = 10.0f;
 
 	// Use this for initialization
 	void Start()
 	{
-		Destroy(gameObject, lifeTime);
+        if (!signal)
+        {
+			Destroy(gameObject, lifeTime);
+		}
 	}
+
+    [PunRPC]
+    public void RPC_DestroyObject()
+    {
+        Destroy(gameObject);
+    }
 }
