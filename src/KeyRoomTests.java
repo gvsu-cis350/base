8fdaf7d9ca45a820bb2f0607b073b1ce33cc1b46
Anna Carvalho <ajcarvalho1201@gmail.com>
2021-10-23 11:08:36 -0400

    Added RoomTests and KeyRoomTests files

---
+++ src/KeyRoomTests.java
@@ -0,0 +1,12 @@
+package demo.tests;
+
+import KeyRoom.java;
+import org.junit.Assert.*;
+
+public class KeyRoomTests {
+
+    @Test
+    public void sample_Test() {
+
+    }
+}
---
+++ src/RoomTests.java
@@ -0,0 +1,12 @@
+package demo.tests;
+
+import Room.java;
+import org.junit.Assert.*;
+
+public class RoomTests {
+    
+    @Test
+    public void sample_Test() {
+
+    }
+}
