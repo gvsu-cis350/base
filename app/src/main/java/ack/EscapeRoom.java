a496c375c306192425088884cc953300f2636fbc
Anna Carvalho <ajcarvalho1201@gmail.com>
2021-12-07 20:21:48 -0500

    changes 'keys' to 'items'

--- app/src/main/java/ack/EscapeRoom.java
+++ app/src/main/java/ack/EscapeRoom.java
@@ -269,12 +269,12 @@ public class EscapeRoom {
         ArrayList<Key> keysToDelete = new ArrayList<>();
 
         if (player.getCurrentPosition().getKeys().size() > 0) {
-            output += "You found the following keys:\n";
+            output += "You found the following items:";
             for (Key k : player.getCurrentPosition().getKeys()) {
                 if (this.searchKeys(k.getName()) == null) {
                     player.addToInventory(k);
                     keysToDelete.add(k);
-                    output += k.getName() + "\n";
+                    output += k.getName();
                 }
             }
         }
